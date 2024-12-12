9913dbdc3e17343298de3557165a6196b23bd6fc
Abdella3 <hweyesso@gmail.com>
2024-12-12 16:02:32 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    
+    File Name: script.js
+    Date: 
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.tron').height($(window).height());
+});
